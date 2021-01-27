2041f168d4133361f441d6e2e91aa15f8457956c
c_melchor <cah.melchor@gmail.com>
2021-01-27 15:12:26 -0800

    repairs item testing working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,5 +1,4 @@
 const enhancer = require('./enhancer.js');
-// test away!
 
 it("testing tests", () => {
     expect(3).toBe(3);
@@ -8,3 +7,20 @@ it("testing tests", () => {
 it("exists", () => {
     expect(enhancer).toBeDefined();
 });
+
+describe("repairs function", () => {
+    beforeEach(() => {
+        // let item = {name, durability, enhancement}
+        enhancer.repair = jest.fn((item) => { return item });
+    });
+
+    it("repairs item", () => {
+        let item = enhancer.repair.item;
+        let repair = enhancer.repair;
+        const expected = item;
+        const actual = repair(item)
+        expect(repair).toBeDefined();
+        expect(actual).toBe(expected)
+
+    });
+})
