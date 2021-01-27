d6c810bddd05fe26e0a28814bbbb07897d05ac39
c_melchor <cah.melchor@gmail.com>
2021-01-27 12:52:41 -0800

    exisits test working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it("testing tests", () => {
+    expect(3).toBe(3);
+});
+
+it("exists", () => {
+    expect(enhancer).toBeDefined();
+});
