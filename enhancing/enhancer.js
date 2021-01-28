e6f9a428aa2767028bc503f3c136b709a97a993a
c_melchor <cah.melchor@gmail.com>
2021-01-27 17:18:20 -0800

    updated enhancer.js

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,38 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
+  get
 };
 
-function success(item) {
-  return { ...item };
+module.exports = item = {
+  name: "item_name",
+  durability: 0,
+  enhancement: 0
 }
 
+function success(item) {
+  if (item.enhancement != 20 && item.enhancement < 20) {
+    return item.enhancement + 1;
+  } else {
+    return { ...item };
+  }
+}
+
+
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return item.durability - 5;
+  } else if (item.enhancement >= 15) {
+    return item.durability - 10;
+  } else if (item.enhancement > 16 && item.enhancement < 21) {
+    return item.enhancement - 1;
+  } else {
+    return { ...item }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, [item.durability]: 100 };
 }
 
 function get(item) {
